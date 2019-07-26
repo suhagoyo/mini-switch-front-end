38f1ab66a7d700c2e161fdbb9764855942b7a412
Suhag <suhaga15@Suhags-MacBook-Pro.local>
2019-07-26 15:46:19 +0530

    deleted files for PR

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const baseUrl = "http://10.1.3.188:8080/rooms/details";
-export const TOGGLE_SWITCH = "TOGGLE_SWITCH";
-export const SET_SELECTED = "SET_SELECTED";
-export  const SET_ROOMS = "SET_ROOMS";
